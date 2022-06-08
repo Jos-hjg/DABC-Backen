95e1819787869242f69b28f8d5f9e185f54032d4
huangjg <huang_learn@163.com>
2022-06-08 14:02:36 +0800

    add change user

--- controllers/users.go
+++ controllers/users.go
@@ -23,19 +23,19 @@ func UserLogin(ctx *gin.Context) {
 		if _, ok := err.(validator.ValidationErrors); ok {
 			ctx.JSON(http.StatusBadRequest, gin.H{
 				"code": http.StatusBadRequest,
-				"msg": models.ValidateError(err.(validator.ValidationErrors), models.LoginValidation),
+				"msg":  models.ValidateError(err.(validator.ValidationErrors), models.LoginValidation),
 			})
 		}
 		return
 	}
 	database.Mysql.Where("username = ?", data.Username).First(&user)
 	fmt.Println(user)
-	pwd := fmt.Sprintf("%x", sha512.Sum512([]byte(data.Password + salt)))
+	pwd := fmt.Sprintf("%x", sha512.Sum512([]byte(data.Password+salt)))
 	fmt.Println(pwd)
 	if pwd != user.Password {
 		ctx.JSON(http.StatusOK, gin.H{
 			"code": http.StatusOK,
-			"msg": "账户不存在或密码错误",
+			"msg":  "账户不存在或密码错误",
 		})
 		return
 	}
@@ -57,7 +57,7 @@ func UserLogin(ctx *gin.Context) {
 	}
 	//log.Println(sign)
 	ctx.JSON(http.StatusOK, gin.H{
-		"code": http.StatusOK,
+		"code":  http.StatusOK,
 		"token": sign,
 		"msg":   "",
 	})
@@ -96,40 +96,106 @@ func CheckAuth(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, gin.H{
 		"code": http.StatusOK,
-		"msg":    "",
+		"msg":  "",
 	})
 }
-func CreateUser(ctx *gin.Context)  {
+func CreateUser(ctx *gin.Context) {
 	salt := "the salt"
 	NewUser := models.User{}
 	if err := ctx.ShouldBind(&NewUser); err != nil {
 		if _, ok := err.(validator.ValidationErrors); ok {
 			ctx.JSON(http.StatusBadRequest, gin.H{
 				"code": http.StatusBadRequest,
-				"msg": models.ValidateError(err.(validator.ValidationErrors), models.CreateValidation),
+				"msg":  models.ValidateError(err.(validator.ValidationErrors), models.CreateValidation),
 			})
 		}
 		return
 	}
 	//TODO: check signature to confirm address
 
-	NewUser.Password = fmt.Sprintf("%x", sha512.Sum512([]byte(NewUser.Password + salt)))
+	NewUser.Password = fmt.Sprintf("%x", sha512.Sum512([]byte(NewUser.Password+salt)))
 	user := models.Users{
 		Username: NewUser.UserName,
 		Nickname: NewUser.NickName,
 		Password: NewUser.Password,
-		Phone: NewUser.Phone,
-		Address: NewUser.Address,
+		Phone:    NewUser.Phone,
+		Address:  NewUser.Address,
 	}
-	if err := database.Mysql.Create(&user).Error; err != nil{
+	if err := database.Mysql.Create(&user).Error; err != nil {
 		ctx.JSON(http.StatusOK, gin.H{
 			"code": http.StatusOK,
-			"msg": err,
+			"msg":  err,
 		})
 		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{
 		"code": http.StatusOK,
-		"msg": "",
+		"msg":  "",
+	})
+}
+
+func ChangeUser(ctx *gin.Context) {
+	headers := ctx.GetHeader("Authorization")
+	log.Println(headers)
+	if haspre := strings.HasPrefix(headers, "dabc-t "); !haspre {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"code": http.StatusUnauthorized,
+			"msg":  "error",
+		})
+		return
+	}
+	if len(headers) <= 7 {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"code": http.StatusUnauthorized,
+			"msg":  "have no authorization",
+		})
+		return
+	}
+	tokenStr := headers[7:]
+
+	//token := sha256.Sum256([]byte("I am a token"))
+	token, _ := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.C.Auth.SignKey), nil
+	})
+
+	if !token.Valid {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"code": http.StatusUnauthorized,
+			"msg":  "token is not available",
+		})
+		return
+	}
+	salt := "the salt"
+	NewUser := models.User{}
+	if err := ctx.ShouldBind(&NewUser); err != nil {
+		if _, ok := err.(validator.ValidationErrors); ok {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"code": http.StatusBadRequest,
+				"msg":  models.ValidateError(err.(validator.ValidationErrors), models.CreateValidation),
+			})
+		}
+		return
+	}
+	//TODO: check signature to confirm address
+
+	NewUser.Password = fmt.Sprintf("%x", sha512.Sum512([]byte(NewUser.Password+salt)))
+	user := models.Users{
+		Username: NewUser.UserName,
+		Nickname: NewUser.NickName,
+		Password: NewUser.Password,
+		Phone:    NewUser.Phone,
+		Address:  NewUser.Address,
+	}
+
+	if err := database.Mysql.Update(&user).Error; err != nil {
+		ctx.JSON(http.StatusOK, gin.H{
+			"code": http.StatusOK,
+			"msg":  err,
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"msg":  "",
 	})
 }
